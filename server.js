5e2806a41464c7625d92343d291c52fe1a4b4d0e
caullens <caullens@ksu.edu>
2017-01-26 14:20:39 -0600

    Added custom pages for each image

--- server.js
+++ server.js
@@ -7,10 +7,45 @@
  */
  
 var http = require('http');
+var fs = require('fs');
 var port = 3000;
 
+function serveImage(filename, req, res) {
+	fs.readFile('images/' + filename, function(err, body) {
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			res.statusMessage = "Server error";
+			res.end();
+			return;
+		}
+		res.setHeader("Content-Type", "image/jpeg");
+		res.end(body);
+	});
+}
+
 var server = http.createServer(function(req, res) {
-	res.end("OK");
+	switch(req.url) {
+		case "/chess":
+			serveImage('chess.jpg', req, res);
+			break;
+		case "/mobile":
+			serveImage('mobile.jpg', req, res);
+			break;
+		case "/ace":
+			serveImage('ace.jpg', req, res);
+			break;
+		case "/bubble":
+			serveImage('bubble.jpg', req, res);
+			break;
+		case "/fern":
+			serveImage('fern.jpg', req, res);
+			break;
+		default:
+			res.statusCode = 404;
+			res.statusMessage = "Not found";
+			res.end();
+	}
 });
 
 server.listen(port, function() {
