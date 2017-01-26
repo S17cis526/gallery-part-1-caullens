dc302820703283cc2d968e121e00cc422cd21ee0
caullens <caullens@ksu.edu>
2017-01-26 13:27:38 -0600

    Simple server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
  
+var http = require('http');
+var port = 3000;
+
+var server = http.createServer(function(req, res) {
+	res.end("OK");
+});
+
+server.listen(port, function() {
+	console.log("Listening on port " + port);
+});
